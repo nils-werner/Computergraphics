0cd25f8546432e2a9d7224ca6911e2d2425d080f
vkb <sivobreu@stud.informatik.uni-erlangen.de>
2010-11-20 20:03:28 +0100

    initial import of A4.3

---
+++ A4.3/A4.3.c
@@ -0,0 +1,27 @@
+/*
+Computergraphics WS2010
+
+Volker Breuer - 21263916
+Nils Werner - 21202217
+Felix Gundlack - 21309819
+*/
+
+#include <GL/glut.h>
+#include <math.h>
+
+void init_openGL() 
+{
+
+}
+
+
+void display()
+{
+
+}
+
+
+int main(int argc, char **argv)
+{
+
+}
