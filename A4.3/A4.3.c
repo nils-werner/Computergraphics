5138fded16397d4a72a6e46f3dae5a8d24f05718
Volker Breuer <volker@vkbmobile.(none)>
2010-11-21 17:31:08 +0100

    in development

--- A4.3/A4.3.c
+++ A4.3/A4.3.c
@@ -10,18 +10,53 @@ Felix Gundlack - 21309819
 #include <math.h>
 
 void init_openGL() 
+
 {
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glFrustum(-1.0, 1.0, -1.0, 1.0, 1.0, 5.0);
 
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0);
 }
-
 
 void display()
 {
+/*
+	glBegin(GL_LINES);
 
+	int i;
+	for(i = 1; i < 20; i++) {
+
+		float degree = 360.0 / i;
+
+		float x = cos(degree);
+		float y = sin(degree);
+
+		glVertex3f(x, y, 0.0);
+	}
+
+
+	glEnd();
+
+*/
+	glColor3f(1.0, 1.0, 0.0);
+	glutSolidSphere(1.3, 25, 25);
+	glFlush();
 }
-
 
 int main(int argc, char **argv)
 {
+	glutInit(&argc, argv);
+	glutInitWindowSize(512, 512);
+	glutCreateWindow("A4.3 - Planeten");
+	glutDisplayFunc(display);
 
+	init_openGL();
+
+	glutMainLoop();
 }
+
