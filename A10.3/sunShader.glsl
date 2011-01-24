c5f607ad0360fcada65162633ed6e4483c4da793
Nils Werner <nils.werner@gmail.com>
2011-01-24 20:13:42 +0100

    Erster Shader, funktioniert noch nicht

--- A10.3/sunShader.glsl
+++ A10.3/sunShader.glsl
@@ -1,7 +1,15 @@
 uniform float Time;  // global time variable from the application
 
+float rand(vec2 co){
+    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
 
-void main()
-{
+void main() {
+	float Rand = rand(vec2(gl_Vertex.y,gl_Vertex.x));
+	float Rad = Time* Rand;
+	
 
+	gl_Position = 
+		gl_ModelViewProjectionMatrix * 
+			(gl_Vertex  + Rad *vec4(gl_Normal, 0)*.1);
 }
